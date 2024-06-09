2570c101f7cf3bde5c82c024dd81cfa104ca80e9
Wagner Aladiah Pacheco de Almeida <wagner.almeida@develoci.com.br>
2024-06-09 12:42:16 -0300

    Animate heroSection arrow down

--- components/hero/HeroSection.tsx
+++ components/hero/HeroSection.tsx
@@ -1,8 +1,43 @@
+"use client";
+
+import { useEffect } from "react";
 import "../../app/homepage.css";
 
-import Cta from "../cta-button";
-
 export default function HeroSection() {
+  const handleArrowClick = () => {
+    const homeDescription = document.getElementById("home-description");
+    const arrowDownDiv = document.getElementById("arrow-down");
+
+    if (homeDescription && arrowDownDiv) {
+      homeDescription.scrollIntoView({ behavior: "smooth" });
+      arrowDownDiv.style.opacity = "0";
+    }
+  };
+
+  const handleScroll = () => {
+    const arrowDownDiv = document.getElementById("arrow-down");
+    if (window.scrollY > 0 && arrowDownDiv) {
+      arrowDownDiv.style.opacity = "0";
+    }
+  };
+
+  useEffect(() => {
+    const arrowDownDiv = document.getElementById("arrow-down");
+
+    if (arrowDownDiv) {
+      arrowDownDiv.addEventListener("click", handleArrowClick);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      if (arrowDownDiv) {
+        arrowDownDiv.removeEventListener("click", handleArrowClick);
+      }
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
       <div className="h-screen flex items-center relative">
@@ -17,7 +52,7 @@ export default function HeroSection() {
         </div>
         <div
           id="arrow-down"
-          className="absolute w-8 bottom-12 left-0 right-0 m-auto"
+          className="absolute w-8 bottom-12 left-0 right-0 m-auto cursor-pointer"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
